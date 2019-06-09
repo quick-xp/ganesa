9bba57f83db949b6acf8d2340c653927f5caf7df
r.nishio <quick.xp@gmail.com>
2019-06-10 08:46:39 +0900

    ticker item

---
+++ src/domain/models/TickerItem.ts
@@ -0,0 +1,26 @@
+export class TickerItem {
+  private _id: number
+  private _bid: number // 買い注文最高値
+  private _ask: number // 売り注文最安値
+  private timestamp: number
+
+  constructor(args: {
+    id?: number
+    bid: number
+    ask: number
+    timestamp: number
+  }) {
+    this._id = args.id
+    this._bid = args.bid
+    this._ask = args.ask
+    this.timestamp = args.timestamp
+  }
+
+  get bid(): number {
+    return this._bid
+  }
+
+  get ask(): number {
+    return this._ask
+  }
+}
